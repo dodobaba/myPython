992f7d67350c3a134581db56b6286adda2198669
Ekain Cao <>
2021-03-25 00:08:39 +0800

    new for NLP

---
+++ tf06.py
@@ -0,0 +1,24 @@
+import json
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+with open("Sarcasm_Headlines_Dataset.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+
+print(padded[:5])
+print(padded.shape)
